165ff9d16cc9af5ce06fed552a9af8fb52b42d41
woozystudio <woozysstudio23@gmail.com>
2025-04-21 08:29:16 -0600

    feat: create ping command

---
+++ commands/ping.ts
@@ -0,0 +1,19 @@
+import { ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+import CubismClient from "../structures/CubismClient.js";
+import { Command, CommandOptions } from "../structures/builders/CommandBuilder.js";
+
+export class PingCommand extends Command<CommandOptions> {
+	constructor() {
+		super({
+			name: "ping",
+			description: "Bot latency",
+			type: ApplicationCommandType.ChatInput,
+		});
+	}
+
+	override execute(interaction: ChatInputCommandInteraction) {
+		interaction.reply(`${CubismClient.client.ws.ping}`);
+	}
+}
+
+export default PingCommand;
