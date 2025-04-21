4d9770f4ab26683bcef242323fb9ea1e8c340919
woozystudio <woozysstudio23@gmail.com>
2025-04-21 08:50:33 -0600

    feat: add alias `CommandInteractionOptions` for `CommandOptions`
    BREAKING CHANGE: `CommandOptions` is now deprecated and should not be used in future code

--- structures/builders/CommandBuilder.ts
+++ structures/builders/CommandBuilder.ts
@@ -1,17 +1,22 @@
 import { ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
 
+/**
+ * @deprecated Use CommandInteractionOptions instead.
+ */
 export type CommandOptions = {
 	name: string;
 	description: string;
 	type: ApplicationCommandType;
 };
 
-export abstract class Command<C extends CommandOptions> {
+export type CommandInteractionOptions = CommandOptions;
+
+export abstract class Command<C extends CommandInteractionOptions> {
 	public name: C["name"];
 	public description: C["description"];
 	public type: C["type"];
 
-	constructor(options: CommandOptions) {
+	constructor(options: CommandInteractionOptions) {
 		this.name = options.name;
 		this.description = options.description;
 		this.type = options.type;
