682cf43a470a58ef3aaedecac3eb20ae604ee38f
woozystudio <woozysstudio23@gmail.com>
2025-04-22 10:07:52 -0600

    fix: options are not created when the command is registered

--- utils/convertCommandsInJSON.ts
+++ utils/convertCommandsInJSON.ts
@@ -12,6 +12,7 @@ export default function convertCommandsInJSON(
 			description: command.description,
 			type: command.type,
 			testOnly: command.testOnly,
+			options: command.options,
 		});
 	});
 
