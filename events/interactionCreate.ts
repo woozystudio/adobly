f985658acb7713c2c9b2853fade598066e74a837
woozystudio <woozysstudio23@gmail.com>
2025-04-23 20:20:35 -0600

    feat: create behaviors for the buttons

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,4 +1,16 @@
-import { ChatInputCommandInteraction, Events } from "discord.js";
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonInteraction,
+	ButtonStyle,
+	ChannelType,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	Events,
+	MessageFlags,
+	PermissionFlagsBits,
+	TextChannel,
+} from "discord.js";
 import { Event, EventCreatorOptions } from "../builders/Event";
 import CubismClient from "../structures/CubismClient";
 
@@ -9,13 +21,81 @@ export default class InteractionCreate extends Event<EventCreatorOptions> {
 		});
 	}
 
-	override execute(interaction: ChatInputCommandInteraction) {
-		if (!interaction.isChatInputCommand()) return;
+	override async execute(interaction: ChatInputCommandInteraction | ButtonInteraction) {
+		if (interaction.isChatInputCommand()) {
+			const command = CubismClient.commands.get(interaction.commandName);
 
-		const command = CubismClient.commands.get(interaction.commandName);
+			if (!command) return;
 
-		if (!command) return;
+			return command.execute(interaction);
+		} else if (interaction.isButton()) {
+			if (interaction.customId === "create-ticket") {
+				const interactionChannel = interaction.channel as TextChannel;
+				const channel = (await interaction.guild?.channels.create({
+					name: `ticket-${interaction.user.username}`,
+					parent: interactionChannel.parentId,
+					type: ChannelType.GuildText,
+					permissionOverwrites: [
+						{
+							id: `${interaction.guildId}`,
+							deny: [
+								PermissionFlagsBits.SendMessages,
+								PermissionFlagsBits.ViewChannel,
+								PermissionFlagsBits.ReadMessageHistory,
+							],
+						},
+						{
+							id: interaction.user.id,
+							allow: [
+								PermissionFlagsBits.SendMessages,
+								PermissionFlagsBits.ViewChannel,
+								PermissionFlagsBits.ReadMessageHistory,
+							],
+						},
+					],
+				})) as TextChannel;
 
-		return command.execute(interaction);
+				const CreatedTicketEmbed = new EmbedBuilder()
+					.setTitle("Welcome! We hope you are well!")
+					.setColor(0xa9d0ff)
+					.setDescription(
+						"A member of the staff will contact you shortly, please be patient. If possible, please leave us your problem or doubt for a faster and more efficient attention.",
+					)
+					.setFooter({
+						iconURL: `${interaction.guild?.iconURL()}`,
+						text: `${interaction.guild?.name} Administration`,
+					})
+					.setTimestamp();
+
+				const TicketManagerButtons = new ActionRowBuilder<ButtonBuilder>().addComponents(
+					new ButtonBuilder()
+						.setCustomId("close-ticket")
+						.setLabel("Close Ticket")
+						.setEmoji("🔒")
+						.setStyle(ButtonStyle.Secondary),
+				);
+
+				channel.send({
+					content: `${interaction.user}`,
+					embeds: [CreatedTicketEmbed],
+					components: [TicketManagerButtons],
+				});
+
+				return interaction.reply({
+					content: `\`✅\` The ticket has been created correctly: ${channel}`,
+					flags: MessageFlags.Ephemeral,
+				});
+			} else if (interaction.customId === "close-ticket") {
+				const interactionChannel = interaction.channel as TextChannel;
+
+				const CloseEmbed = new EmbedBuilder().setDescription("The ticket will be closed soon...").setColor(0xfb2f61);
+
+				setTimeout(() => {
+					interactionChannel.delete();
+				}, 5000);
+
+				return interaction.reply({ embeds: [CloseEmbed] });
+			} else return;
+		} else return;
 	}
 }
