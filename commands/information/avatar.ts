21459e9f0bc2007ed8d9b1875939516c4f97ff8a
woozystudio <woozysstudio23@gmail.com>
2025-05-04 15:23:00 -0600

    feat: create avatar command

---
+++ commands/information/avatar.ts
@@ -0,0 +1,25 @@
+import { ChatInputCommandInteraction, EmbedBuilder, MessageFlags } from "discord.js";
+import { Command } from "../Command.js";
+import { AvatarInteractionCommand } from "../../interactions/avatar.js";
+import i18next from "i18next";
+
+export class AvatarCommand extends Command<typeof AvatarInteractionCommand> {
+	public constructor() {
+		super(AvatarInteractionCommand);
+	}
+
+	override async chatInput(interaction: ChatInputCommandInteraction, locale: string) {
+		const target = interaction.options.getUser("target") || interaction.user;
+		const avatarURL = target.displayAvatarURL({ size: 4096 });
+
+		const rEmbed = new EmbedBuilder()
+			.setTitle(`${i18next.t("command.info.avatar.title", { lng: locale, username: `@${target.username}` })}`)
+			.setURL(avatarURL)
+			.setDescription(avatarURL)
+			.setImage(avatarURL);
+
+		await interaction.reply({ embeds: [rEmbed], flags: MessageFlags.Ephemeral });
+	}
+}
+
+export default AvatarCommand;
---
+++ interactions/avatar.ts
@@ -0,0 +1,17 @@
+import { ApplicationCommandOptionType, ApplicationCommandType } from "discord.js";
+import { CommandInteractionOptions } from "../types/Command";
+
+export const AvatarInteractionCommand: CommandInteractionOptions = {
+	name: "avatar",
+	description: "View a user avatar.",
+	type: ApplicationCommandType.ChatInput,
+	testOnly: true,
+	options: [
+		{
+			name: "target",
+			description: "The user to get the avatar of.",
+			type: ApplicationCommandOptionType.User,
+			required: false,
+		},
+	],
+} as const;
