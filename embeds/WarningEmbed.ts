4650a4bb83a2840a000955c3c76d0134cf4a8d23
woozystudio <woozysstudio23@gmail.com>
2025-05-03 21:20:10 -0600

    feat: create warning embed template

---
+++ embeds/WarningEmbed.ts
@@ -0,0 +1,17 @@
+import EmbedWrapper from "./EmbedWrapper";
+
+export default class WarningEmbed extends EmbedWrapper {
+	/**
+	 *
+	 * @param description The description of the warning message.
+	 */
+	constructor(description: string) {
+		super();
+		this.setDescription(`\`⚠️\` ${description}`);
+		this.setColor(0xffcc4d);
+	}
+
+	static message(description: string) {
+		return new this(description).toMessage(true);
+	}
+}
