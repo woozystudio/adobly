4f71ace1bff5a356bcd703d2968d3af41c7b698d
woozystudio <woozysstudio23@gmail.com>
2025-05-04 14:30:36 -0600

    feat: create guildinfo command

---
+++ interactions/guildinfo.ts
@@ -0,0 +1,9 @@
+import { ApplicationCommandType } from "discord.js";
+import { CommandInteractionOptions } from "../types/Command";
+
+export const GuildInfoInteractionCommand: CommandInteractionOptions = {
+	name: "guildinfo",
+	description: "See server information.",
+	type: ApplicationCommandType.ChatInput,
+	testOnly: true,
+} as const;
