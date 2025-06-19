7d3536a50aeeff1cb5c2da737909b9b3f4a7e721
woozystudio <woozysstudio23@gmail.com>
2025-06-19 11:54:43 -0600

    refactor: refactoring uptime command (#13)

--- commands/utilities/uptime.ts
+++ commands/utilities/uptime.ts
@@ -1,18 +1,17 @@
 import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
-import { Command } from "../Command.js";
 import { UptimeInteractionCommand } from "../../interactions/uptime.js";
-import CubismClient from "../../structures/CubismClient.js";
+import { AdoblyClient, Command } from "@adobly/framework";
 
 export class UptimeCommand extends Command<typeof UptimeInteractionCommand> {
 	constructor() {
 		super(UptimeInteractionCommand);
 	}
 
-	override chatInput(interaction: ChatInputCommandInteraction) {
-		let days = Math.floor((CubismClient.client.uptime as number) / 86400000);
-		let hours = Math.floor((CubismClient.client.uptime as number) / 3600000) % 24;
-		let minutes = Math.floor((CubismClient.client.uptime as number) / 60000) % 60;
-		let seconds = Math.floor((CubismClient.client.uptime as number) / 1000) % 60;
+	override chatInput(interaction: ChatInputCommandInteraction, _locale: string, bot: AdoblyClient) {
+		let days = Math.floor((bot.client.uptime as number) / 86400000);
+		let hours = Math.floor((bot.client.uptime as number) / 3600000) % 24;
+		let minutes = Math.floor((bot.client.uptime as number) / 60000) % 60;
+		let seconds = Math.floor((bot.client.uptime as number) / 1000) % 60;
 
 		const embed = new EmbedBuilder()
 			.setTitle("Uptime")
