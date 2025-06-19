ea5b0f5180a0bb25bdb28c57ff93393dde03e58d
woozystudio <woozysstudio23@gmail.com>
2025-06-19 11:37:13 -0600

    feat: create new client (#13)

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
-import DiscordEventManager from "./events/discord/DiscordEventManager.js";
-import { logger } from "./logger.js";
-import bot from "./structures/CubismClient.js";
+import { AdoblyClient, commandManager, subCommandManager } from "@adobly/framework";
+import { ActivityType, GatewayIntentBits, PresenceUpdateStatus } from "discord.js";
+import dotenv from "dotenv";
+import CommandManager from "./commands/CommandManager";
+import SubCommandManager from "./commands/SubCommandManager";
+dotenv.config();
 
-try {
-	bot.start();
-	bot.registerCommands();
-	new DiscordEventManager().subscribe();
-} catch (error) {
-	try {
-		logger.error(error);
-	} catch (e) {
-		console.error(error);
-	}
-}
+export const client = new AdoblyClient({
+	token: `${process.env.TOKEN}`,
+	id: `${process.env.CLIENT_ID}`,
+	testGuildId: `${process.env.GUILD_ID}`,
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+	presence: {
+		activities: [{ name: "woozystudio.com", type: ActivityType.Custom }],
+		status: PresenceUpdateStatus.Idle,
+	},
+});
+
+client.start();
+
+commandManager.addCommands(CommandManager.commands);
+subCommandManager.addSubCommands(SubCommandManager.subCommands);
