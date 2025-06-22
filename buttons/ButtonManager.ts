fde1f99f4dfb9c108c5b9ab71a1b82b167c2c64e
woozystudio <woozysstudio23@gmail.com>
2025-06-22 11:00:38 -0600

    feat: add buttons to ButtonManager (#14)

--- buttons/ButtonManager.ts
+++ buttons/ButtonManager.ts
@@ -1,9 +1,16 @@
 import { Button, ButtonOptions } from "@adobly/framework";
 import { TicketsConfigurationButton } from "./tickets/tickets-configuration";
 import { TicketsSendMessageButton } from "./tickets/tickets-sendmessage";
+import { CreateTicketButton } from "./tickets/create-ticket";
+import { CloseTicketButton } from "./tickets/close-ticket";
 
 export class ButtonManager {
-	buttons: Button<ButtonOptions>[] = [new TicketsConfigurationButton(), new TicketsSendMessageButton()];
+	buttons: Button<ButtonOptions>[] = [
+		new TicketsConfigurationButton(),
+		new TicketsSendMessageButton(),
+		new CreateTicketButton(),
+		new CloseTicketButton(),
+	];
 }
 
 export default new ButtonManager();
