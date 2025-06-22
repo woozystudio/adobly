06b4bf57bb2e53ea574779a33927a693d867fcd0
woozystudio <woozysstudio23@gmail.com>
2025-06-21 21:40:35 -0600

    feat: refactor setup tickets command and add new config system (#14)

--- commands/utilities/sub/setup-tickets.ts
+++ commands/utilities/sub/setup-tickets.ts
@@ -2,14 +2,15 @@ import {
 	ActionRowBuilder,
 	ButtonBuilder,
 	ButtonStyle,
+	CategoryChannel,
 	ChatInputCommandInteraction,
 	EmbedBuilder,
-	MessageFlags,
 	TextChannel,
 } from "discord.js";
-import { SetupTicketsInteractionSubCommand } from "../../../interactions/sub/setup-tickets.js";
-import i18next from "i18next";
 import { SubCommand } from "@adobly/framework";
+import { SetupTicketsInteractionSubCommand } from "../../../interactions/commands/sub/setup-tickets";
+import TicketSetup from "../../../mongo/TicketSetup";
+import i18next from "i18next";
 
 export class SetupTicketsSubCommand extends SubCommand<typeof SetupTicketsInteractionSubCommand> {
 	constructor() {
@@ -17,29 +18,49 @@ export class SetupTicketsSubCommand extends SubCommand<typeof SetupTicketsIntera
 	}
 
 	override async chatInput(interaction: ChatInputCommandInteraction, locale: string) {
-		const channel = (interaction.options.getChannel("channel") || interaction.channel) as TextChannel;
+		const channel = interaction.options.getChannel("channel") as TextChannel;
+		const category = interaction.options.getChannel("category") as CategoryChannel;
 
-		const TicketCreateEmbed = new EmbedBuilder()
+		let data = await TicketSetup.findOne({ GuildID: interaction.guildId }, { new: true, upsert: true });
+
+		if (!data) {
+			data = await TicketSetup.create({
+				GuildID: interaction.guildId,
+				ChannelID: channel.id,
+				ParentID: category.id,
+			});
+		} else {
+			await TicketSetup.findOneAndUpdate({
+				GuildID: interaction.guildId,
+				ChannelID: channel.id,
+				ParentID: category.id,
+			});
+		}
+
+		const ConfigurationEmbed = new EmbedBuilder()
 			.setAuthor({
 				name: `${i18next.t("command.utility.setupticket.createembed.author", { lng: locale, guild_name: interaction.guild?.name })}`,
 			})
-			.setDescription(`${i18next.t("command.utility.setupticket.createembed.description", { lng: locale })}`)
-			.setThumbnail(`${interaction.guild?.iconURL()}`)
-			.setColor("Blurple");
+			.setColor(0x00d26a)
+			.setDescription(
+				'¡Ya casi acabamos! Si deseas configurar el mensaje de creación de tickets, da click en el botón de "Configuración Avanzada". Abrirá un menu con las opciones para personalizar tu mensaje.',
+			)
+			.setTimestamp();
 
-		const CreateTicketButton = new ActionRowBuilder<ButtonBuilder>().addComponents(
+		const ConfigurationButtons = new ActionRowBuilder<ButtonBuilder>().addComponents(
 			new ButtonBuilder()
-				.setCustomId("create-ticket")
-				.setLabel(`${i18next.t("command.utility.setupticket.createbutton", { lng: locale })}`)
-				.setEmoji("📥")
+				.setCustomId("tickets-configuration")
+				.setLabel("Configuración Avanzada")
+				.setEmoji("⚙️")
 				.setStyle(ButtonStyle.Secondary),
+			new ButtonBuilder()
+				.setCustomId("tickets-sendmessage")
+				.setLabel("Mandar Mensaje")
+				.setEmoji("✅")
+				.setStyle(ButtonStyle.Success),
 		);
 
-		await interaction.reply({
-			content: `${i18next.t("command.utility.setupticket.success", { lng: locale })}`,
-			flags: MessageFlags.Ephemeral,
-		});
-		await channel.send({ embeds: [TicketCreateEmbed], components: [CreateTicketButton] });
+		await interaction.reply({ embeds: [ConfigurationEmbed], components: [ConfigurationButtons] });
 	}
 }
 
