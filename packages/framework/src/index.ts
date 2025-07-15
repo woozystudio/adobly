96fc195b4baccf42ca062d2d6dd5ed36e6a3bdd0
woozystudio <woozysstudio23@gmail.com>
2025-07-14 18:09:12 -0600

    feat: exports util functions

--- packages/framework/src/index.ts
+++ packages/framework/src/index.ts
@@ -1,6 +1,14 @@
-/* Lib */
+// Client
 export * from "./lib/AdoblyClient.js";
+
+// Structures
 export * from "./lib/structures/Command.js";
 
-/* Types */
+// Utils
+export * from "./lib/utils/getAllFiles.js";
+export * from "./lib/utils/getLocalCommands.js";
+export * from "./lib/utils/registerApplicationCommands.js";
+export * from "./lib/utils/translateToJSON.js";
+
+// Types
 export * from "./types/Command.js";
