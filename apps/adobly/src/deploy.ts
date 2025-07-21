73792d18fcd4f409c786b3b21bce1c1514ff00ec
woozystudio <woozysstudio23@gmail.com>
2025-07-21 16:37:03 -0600

    feat: process exit on deploy commands

--- apps/adobly/src/deploy.ts
+++ apps/adobly/src/deploy.ts
@@ -2,3 +2,4 @@ import { registerApplicationCommands } from "@adobly/framework";
 import { client } from "./index.js";
 
 await registerApplicationCommands(client, client.testGuildID);
+process.exit(0);
